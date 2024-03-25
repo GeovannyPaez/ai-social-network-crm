1473bcdf69aceca2d7b808712ce5870b437d1897
Geovanny Paez <gpaez@ab-sistemas.com>
2024-03-25 18:51:24 -0500

    Ignore requests to settings route

--- client/src/pages/Settings/index.jsx
+++ client/src/pages/Settings/index.jsx
@@ -1,70 +1,70 @@
-import  { useState, useEffect } from "react";
-import openSocket from "../../services/socket-io.js";
+// import { useState, useEffect } from "react";
+// import openSocket from "../../services/socket-io.js";
 
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
-import Select from "@mui/material/Select";
-import { toast } from "react-toastify";
+// import Select from "@mui/material/Select";
+// import { toast } from "react-toastify";
 import manualDocuement from "../../assets/manual.pdf"
-import api from "../../services/api.js";
+// import api from "../../services/api.js";
 import { i18n } from "../../translate/i18n.js";
-import toastError from "../../errors/toastError.js";
+// import toastError from "../../errors/toastError.js";
 import { Button } from "@mui/material";
 import { PaperSettings, RootSettings } from "../../components/StyledComponents/index.jsx";
 
 const Settings = () => {
 
-	const [settings, setSettings] = useState([]);
+	// const [settings, setSettings] = useState([]);
 
-	useEffect(() => {
-		const fetchSession = async () => {
-			try {
-				const { data } = await api.get("/settings");
-				setSettings(data);
-			} catch (err) {
-				toastError(err);
-			}
-		};
-		fetchSession();
-	}, []);
+	// useEffect(() => {
+	// 	const fetchSession = async () => {
+	// 		try {
+	// 			const { data } = await api.get("/settings");
+	// 			setSettings(data);
+	// 		} catch (err) {
+	// 			toastError(err);
+	// 		}
+	// 	};
+	// 	fetchSession();
+	// }, []);
 
-	useEffect(() => {
-		const socket = openSocket();
+	// useEffect(() => {
+	// 	const socket = openSocket();
 
-		socket.on("settings", data => {
-			if (data.action === "update") {
-				setSettings(prevState => {
-					const aux = [...prevState];
-					const settingIndex = aux.findIndex(s => s.key === data.setting.key);
-					aux[settingIndex].value = data.setting.value;
-					return aux;
-				});
-			}
-		});
+	// 	socket.on("settings", data => {
+	// 		if (data.action === "update") {
+	// 			setSettings(prevState => {
+	// 				const aux = [...prevState];
+	// 				const settingIndex = aux.findIndex(s => s.key === data.setting.key);
+	// 				aux[settingIndex].value = data.setting.value;
+	// 				return aux;
+	// 			});
+	// 		}
+	// 	});
 
-		return () => {
-			socket.disconnect();
-		};
-	}, []);
+	// 	return () => {
+	// 		socket.disconnect();
+	// 	};
+	// }, []);
 
-	const handleChangeSetting = async e => {
-		const selectedValue = e.target.value;
-		const settingKey = e.target.name;
+	// const handleChangeSetting = async e => {
+	// 	const selectedValue = e.target.value;
+	// 	const settingKey = e.target.name;
 
-		try {
-			await api.put(`/settings/${settingKey}`, {
-				value: selectedValue,
-			});
-			toast.success(i18n.t("settings.success"));
-		} catch (err) {
-			toastError(err);
-		}
-	};
+	// 	try {
+	// 		await api.put(`/settings/${settingKey}`, {
+	// 			value: selectedValue,
+	// 		});
+	// 		toast.success(i18n.t("settings.success"));
+	// 	} catch (err) {
+	// 		toastError(err);
+	// 	}
+	// };
 
-	const getSettingValue = key => {
-		const { value } = settings.find(s => s.key === key);
-		return value;
-	};
+	// const getSettingValue = key => {
+	// 	const { value } = settings.find(s => s.key === key);
+	// 	return value;
+	// };
 
 	return (
 		<RootSettings>
@@ -72,7 +72,7 @@ const Settings = () => {
 				<Typography variant="h4" color="textPrimary" gutterBottom>
 					{i18n.t("settings.title")}
 				</Typography>
-				<PaperSettings>
+				{/* <PaperSettings>
 					<Typography variant="body1">
 						{i18n.t("settings.settings.userCreation.name")}
 					</Typography>
@@ -97,7 +97,7 @@ const Settings = () => {
 					</Select>
 				</PaperSettings>
 
-				{/* <Paper className={classes.paper}>
+				<Paper className={classes.paper}>
 					<TextField
 						id="api-token-setting"
 						readonly
