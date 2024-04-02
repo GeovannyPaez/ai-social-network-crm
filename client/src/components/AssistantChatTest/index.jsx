8695b16d430784c8733cf32ff965aec67ce25753
Geovanny Paez <gpaez@ab-sistemas.com>
2024-04-02 07:36:18 -0500

    streamen reader separate

--- client/src/components/AssistantChatTest/index.jsx
+++ client/src/components/AssistantChatTest/index.jsx
@@ -5,18 +5,8 @@ import RestartAltIcon from '@mui/icons-material/RestartAlt';
 import { getAiRespose } from '../../services/asistanService';
 import toastError from '../../errors/toastError';
 import { i18n } from '../../translate/i18n';
+import streamReader from '../../services/streamReader';
 
-async function* streamReader(response) {
-    const stream = response.body;
-    const reader = stream.getReader();
-    const decoder = new TextDecoder();
-    while (true) {
-        const { value, done } = await reader.read();
-        if (done) break;
-        const decodedChunck = decoder.decode(value, { stream: true });
-        yield decodedChunck;
-    }
-}
 export default function AssistantChatTest() {
     const [messages, setMessages] = useState([]);
     const [inputMessage, setInputMessage] = useState('');
